7c7461cb79f2c4577a54f04be02f158c7160d3ef
Robbert Müller <spam.me@grols.ch>
2017-08-19 10:45:36 +0200

    reformat

--- src/Factory.php
+++ src/Factory.php
@@ -1,26 +1,28 @@
 <?php
 namespace MJRider\FlysystemFactory;
-use \arc\url as url;	
+use \arc\url as url;
 
-class Factory {
+class Factory
+{
 
-	public static function create ($endpoint) {
-		$url = url::url($endpoint);
-		$filesystem = null;
+    public static function create($endpoint)
+    {
+        $url = url::url($endpoint);
+        $filesystem = null;
 
-		switch($url->scheme) {
-		case 'local':
-			break;
-		case 's3':
-			$adapter = S3::create($url);
-			var_dump($adapter);
-			break;
-		default:
-			// TODO fix our own exception handling
-			var_dump($endpoint,$url);
-			throw new \Exception(sprintf('Unknown scheme [%s]',$url->scheme));
-		}
-		return $filesystem;
-	}
+        switch($url->scheme) {
+        case 'local':
+            break;
+        case 's3':
+            $adapter = S3::create($url);
+            var_dump($adapter);
+            break;
+        default:
+            // TODO fix our own exception handling
+            var_dump($endpoint, $url);
+            throw new \Exception(sprintf('Unknown scheme [%s]', $url->scheme));
+        }
+        return $filesystem;
+    }
 }
 
--- src/S3.php
+++ src/S3.php
@@ -1,30 +1,32 @@
 <?php
 namespace MJRider\FlysystemFactory;
-use \arc\url as url;	
+use \arc\url as url;
 use \arc\path as path;
 use \Aws\S3\S3Client;
 use \League\Flysystem\AwsS3v3\AwsS3Adapter;
 
-class S3 {
-	public static function create ($url) {
-		$args = [
-			'credentials' => [
-				'key'    => $url->user,
-				'secret' => $url->pass
-			],
-			'region' => $url->host,
-			'version' => 'latest'
-		];
-		if(isset($url->query->endpoint)) {
-			$args['endpoint'] = $url->query->endpoint;
-		}
-		$bucket  = \arc\path::head( $url->path );
-		$subpath = \arc\path::tail( $url->path );
+class S3
+{
+    public static function create($url)
+    {
+        $args = [
+        'credentials' => [
+        'key'    => $url->user,
+        'secret' => $url->pass
+        ],
+        'region' => $url->host,
+        'version' => 'latest'
+        ];
+        if(isset($url->query->endpoint)) {
+            $args['endpoint'] = $url->query->endpoint;
+        }
+        $bucket  = \arc\path::head($url->path);
+        $subpath = \arc\path::tail($url->path);
 
-		$client = S3Client::factory($args);
+        $client = S3Client::factory($args);
 
-		$adapter = new AwsS3Adapter($client, $bucket, $subpath);
-		return $adapter;
-	}
+        $adapter = new AwsS3Adapter($client, $bucket, $subpath);
+        return $adapter;
+    }
 }
 
