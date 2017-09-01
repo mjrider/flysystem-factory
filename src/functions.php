3b1e7a294ffca57afd404822a5b9e8d36f960bce
Robbert Müller <spam.me@grols.ch>
2017-09-01 15:05:06 +0200

    autofix indenting

--- src/functions.php
+++ src/functions.php
@@ -18,20 +18,20 @@ function create($endpoint)
     $adapter = null;
 
     switch ($url->scheme) {
-    case 's3':
-        $adapter = Adapter\S3::create($url);
-        break;
-    case 'b2':
-        $adapter = Adapter\B2::create($url);
-        break;
-    case 'file':
-    case 'local':
-        $adapter = Adapter\Local::create($url);
-        break;
-    default:
-        // TODO fix our own exception handling
-        var_dump($endpoint, $url);
-        throw new \Exception(sprintf('Unknown scheme [%s]', $url->scheme));
+        case 's3':
+            $adapter = Adapter\S3::create($url);
+            break;
+        case 'b2':
+            $adapter = Adapter\B2::create($url);
+            break;
+        case 'file':
+        case 'local':
+            $adapter = Adapter\Local::create($url);
+            break;
+        default:
+            // TODO fix our own exception handling
+            var_dump($endpoint, $url);
+            throw new \Exception(sprintf('Unknown scheme [%s]', $url->scheme));
     }
     if (!is_null($adapter)) {
         $filesystem = new Filesystem($adapter);
